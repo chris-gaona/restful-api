3733a380d1158e83ee67f27edbdb13cc351be9f2
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-18 11:32:17 -0700

    Finishes basic json token authentication using scotch.io tutorial

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/setup', function(req, res) {
 // =============================================================================
 var apiRoutes = express.Router();              // get an instance of the express Router
 
-// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
 apiRoutes.post('/authenticate', function(req, res) {
   // find the user
   Bear.findOne({
@@ -101,6 +101,31 @@ apiRoutes.post('/authenticate', function(req, res) {
 });
 
 // TODO: route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+});
 
 apiRoutes.use(function(req, res, next) {
   //do logging
