db64d614cae0552e682afd66df488ccb43da3c10
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-18 11:17:12 -0700

    Creates initial api routes...will authenticate api routes next

---
+++ config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+
+    'secret': 'tokenapiauth',
+    'database': 'mongodb://localhost/restful_api'
+
+};
--- server.js
+++ server.js
@@ -8,46 +8,116 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var Bear   = require('./app/models/user'); // get our mongoose model
 
-var port = process.env.PORT || 8080;        // set our port
+// =======================
+// configuration =========
+// =======================
 
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/restful_api', function(err) {
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database, function(err) { // connect to database
   if (err) {
     console.log('Failed connecting to Mongodb!');
   } else {
     console.log('Successfully connected to Mongodb!');
   }
 });
+app.set('superSecret', config.secret); // secret variable
 
-var Bear = require('./app/models/bear');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+app.get('/', function(req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new Bear({
+    name: 'Nick Cerminara',
+    password: 'password',
+    admin: true
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var apiRoutes = express.Router();              // get an instance of the express Router
 
-router.use(function(req, res, next) {
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+  // find the user
+  Bear.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
+// TODO: route middleware to verify a token
+
+apiRoutes.use(function(req, res, next) {
   //do logging
   console.log('Something is happening!');
   next(); //make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+apiRoutes.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
 // more routes for our API will happen here
 
 // on routes that end in /bears
 // ----------------------------------------------------
-router.route('/bears')
+apiRoutes.route('/bears')
   // create a bear (accessed at POST http://localhost:8080/api/bears)
   .post(function(req, res) {
     var bear = new Bear(); // create a new instance of the Bear model
@@ -63,7 +133,7 @@ router.route('/bears')
   })
 
   .get(function(req, res) {
-    Bear.find(function(err, bears) {
+    Bear.find({}, function(err, bears) {
       if (err)
         res.send(err);
 
@@ -73,7 +143,7 @@ router.route('/bears')
 
 // on routes that end in /bears/:bear_id
 // ----------------------------------------------------
-router.route('/bears/:bear_id')
+apiRoutes.route('/bears/:bear_id')
 
   // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
   .get(function(req, res) {
@@ -118,7 +188,7 @@ router.route('/bears/:bear_id')
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', apiRoutes);
 
 // START THE SERVER
 // =============================================================================
