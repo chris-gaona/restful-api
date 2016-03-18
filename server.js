fd757beff99fc1f944f87d4a4009b3c3fb3bfd0e
Chris Gaona <thebeloved88@sbcglobal.net>
2016-03-17 21:47:48 -0700

    Creates restful api using scotch.io tutorial

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 router.use(function(req, res, next) {
   //do logging
-  console.log('Something is happending!');
+  console.log('Something is happening!');
   next(); //make sure we go to the next routes and don't stop here
 });
 
@@ -59,6 +59,60 @@ router.route('/bears')
         res.send(err);
 
       res.json({message: 'Bear created!'});
+    });
+  })
+
+  .get(function(req, res) {
+    Bear.find(function(err, bears) {
+      if (err)
+        res.send(err);
+
+      res.json(bears);
+    });
+  });
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+router.route('/bears/:bear_id')
+
+  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+  .get(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+
+      res.json(bear);
+    });
+  })
+
+  // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+  .put(function(req, res) {
+    // use our bear model to find the bear we want
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err)
+        res.send(err);
+
+      bear.name = req.body.name; // update the bears info
+
+      //save the bear
+      bear.save(function(err) {
+        if (err)
+          res.send(err)
+
+        res.json({message: 'Bear updated!'});
+      });
+    });
+  })
+
+  // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+  .delete(function(req, res) {
+    Bear.remove({
+      _id: req.params.bear_id
+    }, function(err, bears) {
+      if (err)
+        res.send(err);
+
+      res.json({message: 'Successfully deleted!'});
     });
   });
 
